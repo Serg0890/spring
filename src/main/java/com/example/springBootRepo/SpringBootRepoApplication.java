b8452d475b8033016f2813e730ddd34ee9923112
Sergei Starnikov <serg0890@mail.ru>
2025-03-24 14:52:04 +0700

    one commit

---
+++ src/main/java/com/example/springBootRepo/SpringBootRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springBootRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRepoApplication.class, args);
+	}
+
+}
